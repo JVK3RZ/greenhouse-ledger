9543386a664ac2a19311769873e5c90a02bebe49
JVK3RZ <adrienf.vms@gmail.com>
2026-08-13 11:29:47 -0400

    Keep Phase 19 cache validation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'greenhouse-ledger-v19-version-invite-guidance';
+const CACHE_NAME = 'greenhouse-ledger-v19-record-corrections-invite-guidance';
 const APP_SHELL = [
   './',
   './index.html',
